3303f749987b1567f01883fad51f807accaeb3a9
Stypox <stypox@pm.me>
2024-01-23 22:12:17 +0100

    Add vision-related training/visualization scripts

---
+++ vision/generate_annotations.py
@@ -0,0 +1,123 @@
+import json
+import glob
+import os
+import random
+
+# the letter mapping is:
+# H: all top squares have a circle, the base is high
+# L: all top squares have a circle, the base is low
+# T: only half of the top squares have a circle, the other part a half-triangle
+# U: only half of the top squares have a circle, the other part is U-shaped
+VERTICES_TO_CATEGORIES = {
+    1971: (0,  "1x1_H"), # assign1/scene1/view=0
+    2691: (1,  "2x1_T"), # assign1/scene2/view=0
+    2609: (2,  "2x1_L"), # assign1/scene4/view=0
+    3536: (3,  "2x1_H"), # assign1/scene13/view=0
+    2881: (4,  "2x1_U"), # assign1/scene16/view=0
+    6281: (5,  "2x2_H"), # assign1/scene19/view=0
+    5306: (6,  "2x2_U"), # assign1/scene3/view=0
+    5209: (7,  "3x1_H"), # assign1/scene6/view=0
+    3396: (8,  "3x1_U"), # assign1/scene5/view=0
+    6716: (9,  "4x1_H"), # assign1/scene11/view=0
+    4711: (10, "4x1_L"), # assign1/scene30/view=0
+}
+
+def get_views(root):
+    assigns = []
+    for i in range(1,4):
+        assign = []
+        for scenepath in glob.glob(os.path.join(root, f"assign{i}/scene*")):
+            scene = []
+            for view in glob.glob(os.path.join(scenepath, "view=*.json")):
+                scene.append(os.path.relpath(view[:-5], root))
+            scene = sorted(scene, key=lambda a: int(a.split("view=")[-1]))
+            assign.append(scene)
+        assign = sorted(assign, key=lambda a: int(a[0].split("scene")[-1].split("/")[0]))
+        assigns.append(assign)
+    return assigns
+
+def flatten_2(arr):
+    return [c for a in arr for b in a for c in b]
+
+def get_coco_json(root, paths):
+    images = []
+    annotations = []
+    # vertices_to_categories = {}
+
+    for path in paths:
+        image_id = len(images)
+        print(image_id, end="\r")
+        images.append({
+            "id": image_id,
+            "file_name": path + ".jpeg",
+            "height": 1024,
+            "width": 1024,
+        })
+
+        data = json.load(open(os.path.join(root, path + ".json")))
+        for obj in data.values():
+            # use the number of vertices to understand the corresponding block type
+            vh = len(obj["vertices"])
+            category_id = VERTICES_TO_CATEGORIES[vh][0]
+
+            x1,y1,x2,y2 = 1024,1024,0,0
+            for (x,y) in obj["vertices"]:
+                x1 = min(x1, x)
+                y1 = min(y1, y)
+                x2 = max(x2, x)
+                y2 = max(y2, y)
+
+            # bbox3d = obj["vertices"]
+            # from PIL import Image, ImageDraw
+            # from matplotlib import pyplot as plt
+            # image = Image.open(os.path.join(root, path + ".jpeg"))
+            # draw = ImageDraw.Draw(image, "RGBA")
+            # for point in bbox3d:
+            #     draw.point(point)
+            # plt.imshow(image)
+            # plt.show()
+            # exit(0)
+
+            annotations.append({
+                "id": len(annotations),
+                "image_id": image_id,
+                "category_id": category_id,
+                # "category_id": 0, # treat everything as a single category
+                "bbox": [x1, y1, x2-x1, y2-y1],
+                "area": (x2-x1)*(y2-y1),
+            })
+
+    categories = []
+    for (vh, (category_id, name)) in VERTICES_TO_CATEGORIES.items():
+        if category_id not in [c["id"] for c in categories]:
+            categories.append({
+                "id": category_id,
+                "name": name,
+            })
+    #categories = [{"id": 0, "name": "C"}] # treat everything as a single category
+
+    print()
+    return {
+        "categories": categories,
+        "images": images,
+        "annotations": annotations,
+    }
+
+def split_train_test(paths, train_over_total_ratio, seed):
+    rng = random.Random(seed)
+    paths = rng.sample(paths, len(paths))
+    mid = int(train_over_total_ratio * len(paths))
+    return paths[:mid], paths[mid:]
+
+def main():
+    root = "assigns"
+    views = get_views(root)
+    paths = flatten_2(views)
+    train, test = split_train_test(paths, 0.75, 2378)
+    print(f"train size: {len(train)}; test size: {len(test)}")
+    for prefix, paths in [("train", train), ("test", test)]:
+        res = get_coco_json(root, paths)
+        json.dump(res, open(os.path.join(root, prefix + "_annotations.coco.json"), "w"), indent=4)
+
+if __name__ == "__main__":
+    main()
---
+++ vision/train_yolos.py
@@ -0,0 +1,163 @@
+import torch
+import torchvision
+import transformers
+import pytorch_lightning
+import glob
+from common import get_id2label_from_annotations_file
+
+
+class CocoDetection(torchvision.datasets.CocoDetection):
+    def __init__(self, img_folder, ann_file, feature_extractor):
+        super(CocoDetection, self).__init__(img_folder, ann_file)
+        self.feature_extractor = feature_extractor
+
+    def __getitem__(self, idx):
+        # read in PIL image and target in COCO format
+        img, target = super(CocoDetection, self).__getitem__(idx)
+
+        # preprocess image and target (converting target to DETR format, resizing + normalization of both image and target)
+        image_id = self.ids[idx]
+        target = {'image_id': image_id, 'annotations': target}
+        encoding = self.feature_extractor(images=img, annotations=target, return_tensors="pt")
+        pixel_values = encoding["pixel_values"].squeeze() # remove batch dimension
+        target = encoding["labels"][0] # remove batch dimension
+
+        return pixel_values, target
+
+
+class YoloS(pytorch_lightning.LightningModule):
+    def __init__(self, model, train_dataloader, val_dataloader, lr, weight_decay, batch_size):
+        super().__init__()
+        # replace COCO classification head with custom head
+        self.model = model
+        # see https://github.com/PyTorchLightning/pytorch-lightning/pull/1896
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.batch_size = batch_size
+        self.train_dataloaderr = train_dataloader
+        self.val_dataloaderr = val_dataloader
+
+    def forward(self, pixel_values):
+        outputs = self.model(pixel_values=pixel_values)
+
+        return outputs
+
+    def common_step(self, batch, batch_idx):
+        pixel_values = batch["pixel_values"]
+        labels = [{k: v.to(self.device) for k, v in t.items()} for t in batch["labels"]]
+
+        outputs = self.model(pixel_values=pixel_values, labels=labels)
+
+        loss = outputs.loss
+        loss_dict = outputs.loss_dict
+
+        return loss, loss_dict
+
+    def training_step(self, batch, batch_idx):
+        loss, loss_dict = self.common_step(batch, batch_idx)
+        # logs metrics for each training_step
+        self.log("loss", loss, prog_bar=True, on_epoch=True, batch_size=self.batch_size)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss, loss_dict = self.common_step(batch, batch_idx)
+        self.log("val_loss", loss, prog_bar=True, batch_size=self.batch_size)
+        if batch_idx == 41:
+            print()
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(
+            self.parameters(),
+            lr=self.lr,
+            weight_decay=self.weight_decay,
+        )
+
+    def train_dataloader(self):
+        return self.train_dataloaderr
+
+    def val_dataloader(self):
+        return self.val_dataloaderr
+
+
+class ModelCheckpointCallback(pytorch_lightning.Callback):
+    def __init__(self, start_from_epoch):
+        super().__init__()
+        self.start_from_epoch = start_from_epoch
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        folder = f"./checkpoints/epoch_{trainer.current_epoch + self.start_from_epoch}"
+        pl_module.model.save_pretrained(folder)
+
+
+HUGGINGFACE_PATH = "hustvl/yolos-small"
+BATCH_SIZE = 1
+
+def main():
+    # load datasets
+    feature_extractor = transformers.YolosImageProcessor.from_pretrained(
+        HUGGINGFACE_PATH,
+        size={"width": 1024, "height": 768},
+    )
+    root = 'assigns'
+    train_dataset = CocoDetection(img_folder=root, ann_file='assigns/train_annotations.coco.json', feature_extractor=feature_extractor)
+    val_dataset = CocoDetection(img_folder=root, ann_file='assigns/test_annotations.coco.json', feature_extractor=feature_extractor)
+    print("Number of training examples:", len(train_dataset))
+    print("Number of validation examples:", len(val_dataset))
+
+    # create dataloaders
+    def collate_fn(batch):
+        pixel_values = [item[0] for item in batch]
+        encoding = feature_extractor.pad(pixel_values, return_tensors="pt")
+        return {
+            "pixel_values": encoding['pixel_values'],
+            "labels": [item[1] for item in batch],
+        }
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, collate_fn=collate_fn, batch_size=BATCH_SIZE, num_workers=15, shuffle=True)
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, collate_fn=collate_fn, batch_size=BATCH_SIZE, num_workers=15)
+
+    # setup id2label
+    id2label = get_id2label_from_annotations_file()
+    print("TRAINING WITH", len(id2label), "LABELS")
+
+    # model setup
+    epochs = glob.glob("./checkpoints/epoch_*")
+    if len(epochs) == 0:
+        start_from_epoch = 0
+        model_inner = transformers.YolosForObjectDetection.from_pretrained(
+            HUGGINGFACE_PATH,
+            num_labels=len(id2label),
+            ignore_mismatched_sizes=True,
+        )
+    else:
+        last_epoch = sorted(epochs, key=lambda e: int(e.split("_")[-1]))[-1]
+        start_from_epoch = int(last_epoch.split("_")[-1]) + 1
+        model_inner = transformers.YolosForObjectDetection.from_pretrained(
+            last_epoch,
+            num_labels=len(id2label),
+            ignore_mismatched_sizes=True,
+        )
+    print("STARTING FROM EPOCH", start_from_epoch)
+    model = YoloS(model_inner, train_dataloader, val_dataloader, lr=0.8e-5, weight_decay=2e-4, batch_size=BATCH_SIZE)
+
+    # training setup
+    torch.set_float32_matmul_precision('medium')
+    checkpoint_callback = ModelCheckpointCallback(start_from_epoch)
+    trainer = pytorch_lightning.Trainer(
+        max_epochs=50,
+        gradient_clip_val=0.1,
+        accumulate_grad_batches=12,
+        log_every_n_steps=5,
+        callbacks=[checkpoint_callback],
+        enable_model_summary=False,
+    )
+
+    # train!
+    try:
+        trainer.fit(model)
+    except ValueError as e:
+        print(e)
+        exit(36)
+
+if __name__ == "__main__":
+    main()
