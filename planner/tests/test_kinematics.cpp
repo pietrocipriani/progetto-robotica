22a40d1debceaff13aba4c3bd6c42d7e35365153
pietrocipriani <cipriani.pietro002@gmail.com>
2024-01-26 13:47:19 +0100

    damped least squares testing

--- planner/tests/test_kinematics.cpp
+++ planner/tests/test_kinematics.cpp
@@ -11,6 +11,7 @@ using namespace kinematics;
 using namespace model;
 
 bool test_movement();
+bool test_path();
 bool test_direct_kinematics();
 bool test_inverse_kinematics();
 bool test_inverse_diff_kinematics();
@@ -21,6 +22,9 @@ int main() {
 
   // Assuming the correct operation of `direct_kinematics`.
   test("inverse kinematics", test_inverse_kinematics);
+
+
+  test("path", test_path);
 
   // TODO: kinematics test
 
@@ -40,8 +44,10 @@ bool test_movement() {
   for (Scalar time = 0.0; time < 60.0 * 2.6; time += dt) {
     Pose velocity(
       Pose::Position::Random().normalized() * 0.01 * dt,
-      Pose::Orientation(Eigen::AngleAxis<Scalar>(0.1 * dt, Pose::Orientation::Vector3::Random().normalized()))
+      // Can only rotate around the z axis.
+      Pose::Orientation(Eigen::AngleAxis<Scalar>(0.1 * dt, Pose::Orientation::Vector3::UnitZ()))
     );
+    // TODO: cannot choose a random path.
 
     const UR5::Configuration config_variation = dpa_inverse_diff_kinematics(robot, velocity, position);
 
@@ -51,10 +57,8 @@ bool test_movement() {
     auto effective_pos = direct_kinematics(robot);
     Scalar error = effective_pos.error(position).norm();
 
-    std::clog << error << std::endl;
-
-    // TODO: calibrate tollerance, error is acceptable due to time quantization.
-    if (error >= 1e-0) {
+    // TODO: calibrate tollerance
+    if (error > 1e-2) {
       std::stringstream buf;
       buf << "Failed at time " << time << " s with error = " << error
           << " linear part error = " << effective_pos.error(position).position.norm()
@@ -101,3 +105,63 @@ bool test_inverse_kinematics() {
 }
 bool test_inverse_diff_kinematics() { return false; }
 
+Pose::Orientation from_zyx_euler_angles(const Pose::Orientation::VectorType& euler_angles) {
+  return Pose::Orientation(
+    Pose::Orientation::AngleAxisType(euler_angles[0], Pose::Orientation::AngleAxisType::VectorType::UnitZ()) *
+    Pose::Orientation::AngleAxisType(euler_angles[1], Pose::Orientation::AngleAxisType::VectorType::UnitY()) *
+    Pose::Orientation::AngleAxisType(euler_angles[2], Pose::Orientation::AngleAxisType::VectorType::UnitX())
+  );   
+}
+
+Pose interpolate(const Pose& from, const Pose& to, Scalar time) {
+  return Pose(from.position * (1 - time) + to.position * time, from.orientation.slerp(time, to.orientation));
+}
+
+bool test_path() {
+  const Pose initial(Pose::Position(0.3, 0.3, 0.1), Pose::Orientation::Identity());
+  const Pose::Position final_position(0.5, 0.5, 0.5);
+  const Pose final[]{
+    {final_position, from_zyx_euler_angles({M_PI / 3, M_PI_2 - 0.1, M_PI / 3})},
+    {final_position, from_zyx_euler_angles({M_PI_4, M_PI_4, M_PI_4})},
+    {final_position, from_zyx_euler_angles({-M_PI_2 + 0.1, M_PI / 3, 2 * M_PI / 3})}
+  };
+
+  constexpr Scalar dt = 0.01;
+  
+  UR5 robot;
+
+  const UR5::Configuration initial_config = inverse_kinematics(robot, initial);
+
+  // TODO: kp, kq
+
+  for (auto& final_pose : final) {
+    std::clog << "-------------------------- NEW BATCH -------------------------" << std::endl;
+
+    robot.config = initial_config;
+
+    Pose current = initial;
+
+    constexpr Scalar max_time = 10;
+    for (Scalar time = 0; time < max_time; time += dt) {
+      Pose next_pos = interpolate(initial, final_pose, time / max_time);
+
+      Pose movement = current.error(next_pos);
+
+      robot.config += dpa_inverse_diff_kinematics(robot, movement, current);
+
+      current = std::move(next_pos);
+    }
+    Pose movement = current.error(final_pose);
+    robot.config += dpa_inverse_diff_kinematics(robot, movement, current);
+    current = final_pose;
+    
+    auto effective_pos = direct_kinematics(robot);
+
+    std::clog << "Final error: " << effective_pos.error(final_pose).norm() << std::endl;
+    std::clog << "Orientation error: " << effective_pos.error(final_pose).orientation.vec().norm() << std::endl;
+    std::clog << "Configuration: " << robot.config.transpose() << std::endl;
+  }
+
+  return true;
+
+}
