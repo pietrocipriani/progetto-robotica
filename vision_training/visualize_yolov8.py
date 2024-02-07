5a088a501c232f3b3191aeb27ff5cab95b54e2f4
Stypox <stypox@pm.me>
2024-02-07 23:08:16 +0100

    Improve scripts for training/visualizing yolov8

--- vision_training/visualize_yolov8.py
+++ vision_training/visualize_yolov8.py
@@ -4,11 +4,17 @@ import cv2
 
 def main():
     # model setup
-    model = YOLO("runs/detect/train2/weights/best.pt")
+    model = YOLO("runs/detect/multiclass_700/weights/best.pt")
 
     # show image
-    model(sys.argv[1], show=True, imgsz=640, line_width=1,
+    res = model(sys.argv[1], show=True, imgsz=640, line_width=1,
         conf=float(sys.argv[2]), iou=float(sys.argv[3]) if len(sys.argv) > 3 else 0.0)
+
+    # for ((x1, y1, x2, y2), label) in zip(res[0].boxes.xyxy, res[0].boxes.cls):
+    #     print(x1.item(),y1.item(),x2.item(),y2.item(),label.item(),res[0].names[int(label)])
+    # print(res[0].boxes.xyxy)
+    # print(res[0].boxes.cls)
+
     while True:
         try:
             k = cv2.waitKey(1) & 0xFF
