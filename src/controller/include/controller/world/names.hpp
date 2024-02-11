d167dae692626794a7d3538b51b74959716dc021
Stypox <stypox@pm.me>
2024-02-11 12:46:20 +0100

    Implement model deleter

---
+++ src/controller/include/controller/world/names.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef _WORLD_NAMES_HPP_
+#define _WORLD_NAMES_HPP_
+
+namespace controller::world {
+
+constexpr const char* PAD_PREFIX = "pad_";
+
+} // namespace controller::world
+
+#endif // _WORLD_NAMES_HPP_
